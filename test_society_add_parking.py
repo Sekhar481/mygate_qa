436336a1fa35d157cf69f9dfa4d820d69f82dc2a
nikhitha <muninikhithaveluru@gmail.com>
2025-06-22 12:32:16 +0530

    added another new line to the sample

---
+++ test_society_add_parking.py
@@ -0,0 +1,12 @@
+from login_page import Login
+from society_parking_list import Society_Parking
+
+if __name__ == "__main__":
+    login_page = Login("sekhar481@gmail.com", "Jahnavi@16")
+    driver = login_page.launch_browser()
+    login_page.login(driver)
+
+    # object creation for society parking
+    parking = Society_Parking(driver)
+    # calling the add parking function
+    parking.add_parking("I-122-FF","I","122-FF",2,2,"nothing special")
